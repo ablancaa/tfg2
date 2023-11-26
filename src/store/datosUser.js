8287c296169147f8ad8a559e740e2c2348b49a7f
ablancaa <88708431+ablancaa@users.noreply.github.com>
2023-11-26 14:44:14 +0100

    Update datosUser.js
    Nuevo getter en pinia

--- src/store/datosUser.js
+++ src/store/datosUser.js
@@ -29,7 +29,9 @@ export const useDataStore  = defineStore('datosUser',{
     },
 
     getters: {
-    
+    getEmail: (state) => {
+            return state.datosUser.email
+        },
     },
 
     actions: {
