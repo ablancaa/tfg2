cfb13a83882a1a426e1c8f58cc87b83f4def853c
ablancaa <ablancaa@gmail.com>
2023-11-27 16:09:39 +0100

    Service worker

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports = {
-  // ...other vue-cli plugin options...
+  transpileDependencies: true,
+  
   pwa: {
     name: 'TicketService',
     themeColor: '#000000',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-   // workboxPluginMode: 'InjectManifest',
-   // workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-     // swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-   // }
+    appleMobileWebAppStatusBarStyle: 'black'
   }
-}
+})
+
