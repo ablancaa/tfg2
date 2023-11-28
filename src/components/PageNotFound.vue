50087c21a110bc9405c61562dbc6657db65dda82
ablancaa <ablancaa@gmail.com>
2023-11-28 19:10:01 +0100

    Página 404 Page not Found

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
+ 
+        <NavBar2/>
+        <div class="container">
+      <br />
+      <div class="titleMark"><span class="pageTitle">Ticket Nº: <span class="nticket">{{ ticket[0].idTicket }}</span></span></div>   
+        <br/><br/><br/><br/>
         <h1>404 Page not Found</h1>
         <router-link to="/dashBoardAdmin">Dashboard</router-link>
-    </div>
+</div>
 </template>
 
 <script setup>
-
+import NavBar2 from '@/components/NavBar2'
 </script>
 
 <style scoped>
