752d9cf2a45eb5901b4cd797aefc6e4b6e01ecf9
ablancaa <ablancaa@gmail.com>
2023-11-28 22:10:17 +0100

    Nuevo gráfico de categorías en componente estadísticas

--- src/components/GraficosEstadisticas.vue
+++ src/components/GraficosEstadisticas.vue
@@ -2,46 +2,47 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-md-6">
-                <div class="bloque-estadiscicas">
-                    <canvas id="tickets"></canvas>
-                </div> <!--Fin Bloque Estadisticas --></div>
+                <router-link to="/ticketsView">
+                    <div class="bloque-estadiscicas">
+                        <canvas id="tickets"></canvas>
+                    </div> <!--Fin Bloque Estadisticas -->
+                </router-link>
+            </div>
+            <div class="col-sm-12 col-md-6">
+                <router-link to="/usersView">
+                    <div class="bloque-estadiscicas">
+                        <canvas id="usuarios"></canvas>
+                    </div> <!--Fin Bloque Estadisticas -->
+                </router-link>
+            </div>
             <div class="col-sm-12 col-md-6">
                 <div class="bloque-estadiscicas">
-                    <canvas id="usuarios"></canvas>
+                    <canvas id="categorias"></canvas>
                 </div> <!--Fin Bloque Estadisticas -->
             </div>
         </div>
     </div>
-
-
-
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 import Chart from 'chart.js/auto';
 
-// const props = defineProps({
-//     contadores: {
-//         type: Object,
-//     },    
-// })
-
 onMounted(() => {
     pintaGraficaTickets();
     pintaGraficaUsuarios();
+    pintaGraficaCategorias();
 });
 
-
 const pintaGraficaTickets = () => {
- 
-    let ticket = JSON.parse( localStorage.tickets );
-    let ticketsNum =  ticket.ticketsNum ;
+
+    let ticket = JSON.parse(localStorage.tickets);
+    let ticketsNum = ticket.ticketsNum;
     let ticketsActive = ticket.ticketsActive;
     let ticketsWait = ticket.ticketsWait;
     let ticketsProgress = ticket.ticketsProgress;
     let ticketsEnd = ticket.ticketsEnd;
-  
+
     const ctx = document.getElementById("tickets");
     const labels = ['Nº Tickets Total', 'En Activo', 'En Espera', 'En proceso', 'Resuelto']
 
@@ -52,7 +53,7 @@ const pintaGraficaTickets = () => {
             datasets: [{
                 display: false,
                 label: 'Tickets',
-                data: [ticketsNum,ticketsActive,ticketsWait,ticketsProgress,ticketsEnd],
+                data: [ticketsNum, ticketsActive, ticketsWait, ticketsProgress, ticketsEnd],
                 borderColor: 'black',
                 pointStyle: 'rectRounded',
                 backgroundColor: 'rgba(9, 129, 176, 0.2)',
@@ -69,38 +70,72 @@ const pintaGraficaTickets = () => {
     });
 }
 const pintaGraficaUsuarios = () => {
- 
- let usuarios = JSON.parse( localStorage.usuarios );
- let usuNum =  usuarios.usersNum ;
- let usuActive = usuarios.usersActive;
- let usuDisconnect = usuarios.usersDisconnect;
- 
 
- const ctx = document.getElementById("usuarios");
- const labels = ['Nº Usuarios Total', 'On Line', 'Off Line']
+    let usuarios = JSON.parse(localStorage.usuarios);
+    let usuNum = usuarios.usersNum;
+    let usuActive = usuarios.usersActive;
+    let usuDisconnect = usuarios.usersDisconnect;
 
- new Chart(ctx, {
-     type: 'bar',
-     data: {
-         labels: labels,
-         datasets: [{
-             display: false,
-             label: 'Usuarios',
-             data: [usuNum, usuActive,usuDisconnect],
-             borderColor: 'black',
-             pointStyle: 'rectRounded',
-             backgroundColor: 'rgba(9, 129, 176, 0.2)',
-             borderWidth: 1
-         }]
-     },
-     options: {
-         scales: {
-             y: {
-                 beginAtZero: true
-             }
-         }
-     }
- });
+
+    const ctx = document.getElementById("usuarios");
+    const labels = ['Nº Usuarios Total', 'On Line', 'Off Line']
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                display: false,
+                label: 'Usuarios',
+                data: [usuNum, usuActive, usuDisconnect],
+                borderColor: 'black',
+                pointStyle: 'rectRounded',
+                backgroundColor: 'rgba(9, 129, 176, 0.2)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+const pintaGraficaCategorias = () => {
+
+    let ticket = JSON.parse(localStorage.tickets);
+    let ticketSoftware = ticket.ticketSofware;
+    let ticketAsistencia = ticket.ticketAsistencia;
+    let ticketHardware = ticket.ticketHardware;
+    let ticketServicios = ticket.ticketServicios;
+
+    const ctx = document.getElementById("categorias");
+    const labels = ['Software', 'Hardware', 'Asistencia', 'Servicios']
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                display: false,
+                label: 'Categorías',
+                data: [ticketSoftware, ticketAsistencia, ticketHardware, ticketServicios],
+                borderColor: 'black',
+                pointStyle: 'rectRounded',
+                backgroundColor: 'rgba(9, 129, 176, 0.2)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
 </script>
 
