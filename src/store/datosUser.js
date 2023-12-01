834c3df6c7a5b19c2dee06a6ab2cfac6058eb066
ablancaa <ablancaa@gmail.com>
2023-12-01 17:48:43 +0100

    Mejoras en código del store

--- src/store/datosUser.js
+++ src/store/datosUser.js
@@ -10,7 +10,18 @@ import { defineStore } from 'pinia'
 export const useDataStore  = defineStore('datosUser',{
     state: () => {
         return {
-            datosUser: [{
+            // datosUser: [{
+            //     idUser: '',
+            //     avatar: '',
+            //     name: '',
+            //     surname1:'',
+            //     surname2:'',
+            //     email: '',
+            //     rol: '',
+            //     phone: '',
+            //     state: false,               
+            // }],
+            currenUser: [{
                 idUser: '',
                 avatar: '',
                 name: '',
@@ -19,57 +30,68 @@ export const useDataStore  = defineStore('datosUser',{
                 email: '',
                 rol: '',
                 phone: '',
-                state: false,               
+                state: false,
+                firebaseRef:''               
             }],
-            userList: [],
+            userList: [{}],
             ticketList: [],
             contadoresUsuario: [],
             
         } 
     },
     persist: {
-        paths: ['datosUser.idUser', 
-                'datosUser.email', 
-                'datosUser.avatar', 
-                'datosUser.rol',
-                'datosUser.name',
-                'datosUser.surname1',
-                'datosUser.surname2',
+        paths: ['currenUser.idUser', 
+                'currenUser.email', 
+                'currenUser.avatar', 
+                'currenUser.rol',
+                'currenUser.name',
+                'currenUser.surname1',
+                'currenUser.surname2',
+                'currenUser.state',
+                'currenUser.firebaseRef',
+                'userList',
+                
             ],
     },
     getters: {
     getEmail: (state) => {
-            return state.datosUser.email
+            return state.currenUser.email
         },
     },
 
     actions: {
         mostrarDatosUsuario(){
-           console.log(this.datosUser.email);
+           console.log(this.currenUser.email);
         },
         setEmail(mail) {
-            this.datosUser.email = mail;
+            this.currenUser.email = mail;
         },
         setAvatar(avatar) {
-            this.datosUser.avatar = avatar;
+            this.currenUser.avatar = avatar;
         },
         setidUser(idUser) {
-            this.datosUser.idUser = idUser;
+            this.currenUser.idUser = idUser;
         },
         setName(name) {
-            this.datosUser.name = name;
+            this.currenUser.name = name;
         },
         setSurname1(surname1) {
-            this.datosUser.surname1 = surname1;
+            this.currenUser.surname1 = surname1;
         },
         setSurname2(surname2) {
-            this.datosUser.surname2 = surname2;
+            this.currenUser.surname2 = surname2;
         },
         setRol(rol) {
-            this.datosUser.rol = rol;
+            this.currenUser.rol = rol;
         },
         setPhone(phone) {
-            this.datosUser.phone = phone;
+            this.currenUser.phone = phone;
+        },
+        setState(state) {
+            this.currenUser.state = state;
+        },
+        setFirebaseRefCurrenUser(ref){
+            this.currenUser.firebaseRef = ref
         },
         setUsersList(lista){
             this.userList=lista
