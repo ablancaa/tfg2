bb5a2c0dfa7ce537de13db75bbe6b5deec47aeb2
ablancaa <ablancaa@gmail.com>
2023-12-06 17:27:30 +0100

    Preparación para recibir mensajes

--- src/utils/FirebaseConfig.js
+++ src/utils/FirebaseConfig.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, setDoc, doc } from "firebase/firestore"
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,4 +27,29 @@ const auth = getAuth(app);
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
 
-export {app, db, auth, collection, getDocs, setDoc, doc, database}
+
+
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+const messaging = getMessaging(app);
+
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload);
+  // ...
+});
+getToken(messaging, { vapidKey: 'BAURg_lDgE3qsPlRKVr3VaGDHj5t5Xe1Gg_n0BN-9EYG8AyNXwAsqBuoHNt86AtjJvSpSXww77Tbry4rpuRTKuU' }).then((currentToken) => {
+  if (currentToken) {
+    // Send the token to your server and update the UI if necessary
+    // ...
+    console.log("token is: ",currentToken)
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
+});
+
+export {app, db, auth, collection, getDocs, setDoc, doc, database, messaging}
