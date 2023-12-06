8a3b66da6bf3a17205e066c0cfac4cb5a056d4b6
ablancaa <ablancaa@gmail.com>
2023-12-06 17:37:57 +0100

    Mejora de código

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,4 @@
- // Give the service worker access to Firebase Messaging.
+// Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
 // are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
