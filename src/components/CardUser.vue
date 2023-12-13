a03bc08b67c73d3e2174bb16c0d85e1dfd894831
ablancaa <ablancaa@gmail.com>
2023-12-13 20:47:20 +0100

    css

--- src/components/CardUser.vue
+++ src/components/CardUser.vue
@@ -59,7 +59,7 @@ import { defineProps } from 'vue';
 }
 
 .flex-items:nth-child(2) {
-    width: 200px;
+    width: 210px;
     justify-content: start;
     align-items:start;
     /* background-color: rgb(28, 196, 196); */
@@ -71,7 +71,7 @@ import { defineProps } from 'vue';
   justify-content: normal;
   align-items: normal;
   align-content: normal;
-  width: 4.9vh;
+  width: 5.9vh;
 }
 
 .flex-item-estado:nth-child(1) {
